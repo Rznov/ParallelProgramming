0bd3d26cacc48d96aee3dcaebab2ecd8e259d3fe
rznov <rznnova@gmail.com>
2020-06-07 22:58:56 +1100

    Fixed read input data in JacobiSolver

--- LinearSystem/JacobiSolver.cpp
+++ LinearSystem/JacobiSolver.cpp
@@ -29,24 +29,22 @@ public:
         double localError = 0, globalError = 0;
         bool fillEnds = false;
 
-        //printf("Hello world from processor rank %d out of %d processors\n", processId, processesNum);
+        vectorSize = new int[processesNum];
+        displacements = new int[processesNum];
 
         if (processId == main_process) {
-            //works till here
             readInputs(coefficientsFilename, estimateFilename);
-            //log(toString(b, size));
             if (!isConvergence()) {
-                cout << "That linear system can't be solved";
                 MPI_Abort(MPI_COMM_WORLD, -1);
             }
         }
 
         MPI_Bcast(&size, 1, MPI_DOUBLE, main_process, MPI_COMM_WORLD);
+        previousX = new double [size];
 
         if (processId != main_process) {
             coefficients = new double [size * size];
             currentX = new double [size];
-            previousX = new double [size];
             b = new double [size];
         }
 
@@ -57,38 +55,39 @@ public:
         if (processId == main_process) {
             int shift = 0, countX;
             for (int proc = 0; proc < processesNum; ++proc) {
-                countX = size / processesNum + (size % processesNum > processId ? 1 : 0);
+                countX = size / processesNum + (size % processesNum > proc ? 1 : 0);
                 vectorSize[proc] = countX;
                 displacements[proc] = shift;
                 shift += countX;
             }
-            //printf("process %d, size %d\n", processId, processesNum);
-            fillEnds = true;
-            //resultToConsole(displacements);
-            MPI_Bcast(&fillEnds, 1, MPI_CXX_BOOL, 0, MPI_COMM_WORLD);
-            MPI_Bcast(vectorSize, processesNum, MPI_INT, 0, MPI_COMM_WORLD);
-            MPI_Bcast(displacements, processesNum, MPI_INT, 0, MPI_COMM_WORLD);
         }
-        while (fillEnds);
+        MPI_Bcast(vectorSize, processesNum, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(displacements, processesNum, MPI_INT, 0, MPI_COMM_WORLD);
 
         int start, finish;
         start = displacements[processId];
         if (processId + 1 == processesNum) {
             finish = size - 1;
         } else {
-            finish = displacements[processId + 1];
+            finish = displacements[processId + 1] - 1;
         }
+        //works till here
 
-        do {
+//        do {
             for (int x = start; x <= finish; ++finish) {
-                previousX = currentX;
-                currentX[x] = getX(previousX, b[x], coefficients[x * size + 1]);
+                //log(to_string(x));
+                saveCurrentXAsPrevious();
+                //log(toString(previousX, size));
+                computeX(x);
+//                log(to_string(currentX[x]));
+//                log(to_string(previousX[x]));
+                break;
                 localError += abs(previousX[x] - currentX[x]);
             }
-            MPI_Allreduce(&localError, &globalError, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-            MPI_Allgatherv(currentX, finish - size, MPI_DOUBLE, previousX, vectorSize, displacements, MPI_DOUBLE, MPI_COMM_WORLD);
-        }
-        while (globalError > error);
+//            MPI_Allreduce(&localError, &globalError, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+//            MPI_Allgatherv(currentX, finish - size, MPI_DOUBLE, previousX, vectorSize, displacements, MPI_DOUBLE, MPI_COMM_WORLD);
+        //}
+//        while (globalError > error);
 
         return previousX;
     }
@@ -99,47 +98,49 @@ private:
         ifstream coefFile(coefficientsFilename);
         int rows, columns;
         coefFile >> rows >> columns;
-//        string rowcol = to_string(rows) + " " + to_string(columns);
-//        log(rowcol);
         size = rows;
         b = new double[size];
         coefficients = new double[rows * columns];
         for (size_t i = 0; i < rows; ++i) {
             for (size_t j = 0; j < columns; ++j) {
                 if (j == columns - 1) {
-                    coefFile >> b[rows];
-                    string bs = to_string(b[rows]);
-                    log(bs);
+                    coefFile >> b[i];
+                    b[i] *= -1;
                     continue;
                 }
                 coefFile >> coefficients[i * rows + j];
-                string cs = to_string(coefficients[i * rows + j]);
-                log(cs);
             }
         }
         coefFile.close();
-        string bs = to_string(b[1]);
-        log(bs);
 
         ifstream estimFile(estimateFilename);
         estimFile >> rows >> columns;
         currentX = new double [rows];
         for (size_t i = 0; i < rows; ++i) {
-            estimFile >> currentX[rows];
+            estimFile >> currentX[i];
         }
         estimFile.close();
     }
 
+    void saveCurrentXAsPrevious() {
+        if (previousX == nullptr) {
+            previousX = new double[size];
+        }
+        for (int i = 0; i < size; i++) {
+            previousX[i] = currentX[i];
+        }
+    }
+
     bool isConvergence() {
         double sum = 0;
-        for (size_t i; i < size; ++i) {
-            for (size_t j; j < size; ++j) {
+        for (size_t i = 0; i < size; ++i) {
+            for (size_t j = 0; j < size; ++j) {
                 if (i != j) {
                     sum += coefficients[i * size + j];
                 }
             }
         }
-        for (size_t i; i < size; ++i) {
+        for (size_t i = 0; i < size; ++i) {
             if (coefficients[i * (size + 1)] < sum) {
                 return false;
             }
@@ -147,16 +148,20 @@ private:
         return true;
     }
 
-    double getX(double *x, double b, double divisor) {
-        double sum = 0, result;
-        for (int j = 0; j < size; ++j) {
-            if (coefficients[j] == divisor) {
+    void computeX(size_t row) {
+        double sum = 0;
+        double divisor = coefficients[row * size + row];
+
+        for (size_t j = 0; j < size; ++size) {
+            double coef = coefficients[row * size + j];
+
+            if (coef == divisor) {
                 continue;
             }
-            sum += coefficients[j] + x[j];
+            sum += coef * previousX[j];
         }
-        result = (b - sum) / divisor;
-        return result;
+
+        currentX[row] = sum / divisor;
     }
 
     void resultToConsole(double *result) {
@@ -172,6 +177,26 @@ private:
     }
 
     static string toString(double *array, int size) {
+        string contentString;
+        if (size < 20) {
+            for (int i = 0; i < size; i++) {
+                contentString += to_string(array[i]);
+                if (i < size - 1) contentString += " ";
+            }
+        } else {
+            for (int i = 0; i < 5; i++) {
+                contentString += to_string(array[i]) + " ";
+            }
+            contentString += " ... ";
+            for (int i = size - 5; i < size; i++) {
+                contentString += to_string(array[i]);
+                if (i < size - 1) contentString += " ";
+            }
+        }
+        return "|" + to_string(size) + "|[" + contentString + "]";
+    }
+
+    static string toString(int *array, int size) {
         string contentString;
         if (size < 20) {
             for (int i = 0; i < size; i++) {
